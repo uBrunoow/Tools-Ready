3e9839dba2accb53368a57dd78ac011b424fa68a
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-03-12 14:13:52 -0300

    fiz o countdown no index

--- JS/finalizar-cadastro.js
+++ JS/finalizar-cadastro.js
@@ -1,7 +1,7 @@
 var currentImgIndex=1;
 var ImgSrcArray = [ //caminho das suas imgs aqui
-'./Images/Vector (1).svg',
-'./Images/Vector (2).svg'
+'../Images/Vector (2).svg',
+'../Images/Vector (1).svg'
 ];
 // FUNCTION PRODUTO 1
 function clickCheck(){
@@ -10,7 +10,24 @@ function clickCheck(){
   {
     currentImgIndex=0;
   }
-  document.getElementById("p510").src=ImgSrcArray[currentImgIndex]; //altera a img do elemento "agni" de acordo com o indice
+  document.getElementById("imagem").src=ImgSrcArray[currentImgIndex]; //altera a img do elemento "agni" de acordo com o indice
+    currentImgIndex++; // incrementa a nossa referencia
+
+}
+
+var currentImgIndex=1;
+var ImgSrcArray = [ //caminho das suas imgs aqui
+'../Images/Vector (2).svg',
+'../Images/Vector (1).svg'
+];
+// FUNCTION PRODUTO 1
+function clickCheck2(){
+
+  if(currentImgIndex == ImgSrcArray.length) //reseta quando o contatador for igual ao tamanho da array e volta a 1° img
+  {
+    currentImgIndex=0;
+  }
+  document.getElementById("imagem2").src=ImgSrcArray[currentImgIndex]; //altera a img do elemento "agni" de acordo com o indice
     currentImgIndex++; // incrementa a nossa referencia
 
 }
